528159ac9cfe8091d1372087ba756aad1809d853
Hexcede <8394472+Hexcede@users.noreply.github.com>
2023-02-19 14:01:50 -0500

    Generate sourcemap differently for compatibility

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 aftman install
 wally install
-rojo sourcemap > ./sourcemap.json
+rojo sourcemap sourcemap-dev.project.json > ./sourcemap.json
 wally-package-types --sourcemap sourcemap.json Packages/
